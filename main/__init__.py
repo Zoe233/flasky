c85ebc4514b2785cdd86fc3101aa425230f1c5b0
Zoe <740465595@qq.com>
2018-04-23 18:41:07 +0800

    需要导入.forms和 ..db,models和decorators等

--- main/__init__.py
+++ main/__init__.py
@@ -1,4 +1,12 @@
 from flask import Blueprint
+'''
+项目模块划分阶段，使用Blueprint('蓝本'）。
+Blueprint通过把实现不同功能的module分开，从而把一个大的application分隔成各自实现不同功能的module.
+在一个Blueprint中可以调用另一个blueprint的view function,但要加相应的blueprint名。
+其他好处：让程序更加松耦合，更加灵活，增加复用性，提高查错效率，降低出错概率。
+在具体项目开发过程中国，不同蓝本分别对应不同的功能模块。
+例如：auth授权模块 和 项目主模块。
+'''
 
 main = Blueprint('main',__name__)
 from . import views,errors
---
+++ main/views.py
@@ -0,0 +1,9 @@
+from flask import render_template, redirect, url_for, abort, flash, \
+    request,current_app,make_response
+from flask_login import login_required, current_user
+from flask_sqlalchemy import get_debug_queries
+from . import main
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm, CommentForm
+from .. import db
+from ..models import Permission, Role, User, Post, Comment
+from ..decorators import admin_required, permission_required
