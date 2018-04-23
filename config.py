4b6d5401cd45eec245d86d8d799ef6d813078f56
Zoe <740465595@qq.com>
2018-04-23 16:40:06 +0800

    config文件：config中配置的环境读取在config参数中，分别有development,testing,production,heroku,docker,unix,default模式。 里面使用方法有getattr, @classmethod, @staticmethod, logging, SMTPHandler, 众多常量

---
+++ config.py
@@ -0,0 +1,131 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER','smtp.goolemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT','587'))
+        # get(self, key, default=None) '587'为'MAIL_PORT'不存在时的默认值
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS','true').lower() in ['true','on','1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    FLASK_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASK_MAIL_SENDER = 'Flask Admin <flask@example.com>'
+    FLASK_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    SSL_REDIRECT = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
+
+    FLASK_POSTS_PER_PAGE = 20
+    FLASK_FOLLOWERS_PER_PAGE = 50
+    FLASK_COMMENTS_PER_PAGE = 30
+    FLASK_SLOW_DB_QUERY_TIME = 0.5
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///'+os.path.join(basedir,'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite://'
+    WTF_CSRF_ENABLED = False
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+
+    @classmethod
+    def init_app(cls,app):
+        Config.init_app(app)
+
+        # email errors to the administrators
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None # 证书
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            # getattr(object, name, default=None) --> value
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.FLASKY_MAIL_SENDER,
+            toaddrs=[cls.FLASKY_ADMIN],
+            subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + 'Application Error',
+            credentials=credentials,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+class HerokuConfig(ProductionConfig):
+    '''
+    Heroku PaaS提供商之一
+    将项目部署到Heroku平台
+    '''
+    SSL_REDIRECT = True if os.environ.get('DYNO') else False
+
+    @classmethod
+    def init_app(cls,app):
+        ProductionConfig.init_app(app)
+
+        # handle reverse proxy server headers
+        from werkzeug.contrib.fixers import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+class DockerConfig(ProductionConfig):
+    '''
+    将项目部署到Docker中
+    '''
+    @classmethod
+    def init_app(cls,app):
+        ProductionConfig.init_app(app)
+
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+
+class UnixConfig(ProductionConfig):
+    '''
+    将项目部署在Unix系统上
+    '''
+    @classmethod
+    def init_app(cls,app):
+        ProductionConfig.init_app(app)
+
+        # log to syslog
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.INFO)
+        app.logger.addHandler(syslog_handler)
+
+
+config = {
+    'development':DevelopmentConfig,
+    'testing':TestingConfig,
+    'production':ProductionConfig,
+    'heroku':HerokuConfig,
+    'docker':DockerConfig,
+    'unix':UnixConfig,
+    'default':DevelopmentConfig
+}
